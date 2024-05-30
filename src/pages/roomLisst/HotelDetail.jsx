ae7d95623a40c1ad768ceb1ee09cead7dd6324e0
SE172533_TrongKhang <metchetme123@gmail.com>
2024-05-30 09:05:40 +0700

    update

--- src/pages/roomLisst/HotelDetail.jsx
+++ src/pages/roomLisst/HotelDetail.jsx
@@ -9,14 +9,13 @@ import HeaderHotel from './component/jsx/HeaderHotel';
 import SlideShow from './component/jsx/SlideShow';
 import IntroductionHotel from './component/jsx/IntroductionHotel';
 import Roomlist from './component/jsx/Roomlist';
+import HotelAbout from './component/jsx/HotelAbout';
 
 const RoomlistDetail = () => {
     const { hotelId } = useParams('hotelId');
     const { data: hotelDetail, isLoading } = useGetHotelByIdQuery(hotelId);
     const bigImages = hotelDetail?.image?.slice(0, 6);
     const smallImages = hotelDetail?.image?.slice(0, 4);
-
-
 
 
     const HeaderParams = {
@@ -41,7 +40,10 @@ const RoomlistDetail = () => {
     const RoomListParams = {
         hotelId: hotelId
     }
-
+    const HotelDescription = {
+        hotelDescription: hotelDetail?.description,
+        hotelImage: hotelDetail?.image?.slice(0, 3)
+    }
 
 
 
@@ -49,6 +51,8 @@ const RoomlistDetail = () => {
     if (isLoading) {
         return <h1>Loading...</h1>;
     }
+
+    
     return (
         <>
             <Row style={{ marginTop: '6rem', width: "100%" }}>
@@ -56,6 +60,7 @@ const RoomlistDetail = () => {
                 <SlideShow {...SlideParams} />
                 <IntroductionHotel {...IntroductionParams} />
                 <Roomlist {...RoomListParams} />
+                <HotelAbout {...HotelDescription} />
             </Row>
         </>
     );
