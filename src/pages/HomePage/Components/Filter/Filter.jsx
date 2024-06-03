a28068ababac4ad9808ed187fa7e153eb0c310e9
lamquocvinh <lamvinh940@gmail.com>
2024-06-03 09:30:38 +0700

    fix ui/ux and do profile

--- src/pages/HomePage/Components/Filter/Filter.jsx
+++ src/pages/HomePage/Components/Filter/Filter.jsx
@@ -1,14 +1,79 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import "./Filter.scss"
-import { Card, Col, Input, Row, Select } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
+import { Card, Col, Input, Row, Select, InputNumber, Form, Popover, Button, Alert, notification } from "antd";
+import { SearchOutlined, UserOutlined } from "@ant-design/icons";
 import IMG from '../../../../assets/photo-3-1485152074061.jpg';
 import { VietnameseProvinces } from "../../../../utils/utils";
+import { DatePicker, Space } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { setGuests, setRooms, setDestination, setDate } from '../../../../slices/hotelSearch.slice';
+dayjs.extend(customParseFormat);
+const { RangePicker } = DatePicker;
+const dateFormat = 'DD/MM/YYYY';
+const disabledDate = (current) => {
+    // Can not select days before today and today
+    return current && current < dayjs().endOf('day');
+};
+
+
 
 const Filter = () => {
-    const [keywords, setKeywords] = useState('');
-    const [destination, setDestination] = useState('');
-    const [duration, setDuration] = useState('');
+    const [visible, setVisible] = useState(false);
+    // const [guests, setGuests] = useState(1);
+    // const [rooms, setRooms] = useState(1);
+    // const [destination, setDestination] = useState(null);
+    // const [date, setDate] = useState(null);
+    const dispatch = useDispatch();
+    const guests = useSelector((state) => state.hotelSearch.guests);
+    const rooms = useSelector((state) => state.hotelSearch.rooms);
+    const date = useSelector((state) => state.hotelSearch.date);
+    const destination = useSelector((state) => state.hotelSearch.destination);
+    const handleRoomsChange = (value) => {
+        dispatch(setRooms(value));
+        if (guests > value * 2) {
+            dispatch(setGuests(value * 2));
+        }
+    };
+
+    const handleGuestsChange = (value) => {
+        if (value > rooms * 2) {
+            notification.warning({
+                message: "Warning!",
+                description: "Mỗi phòng tối đa 2 người."
+            })
+            dispatch(setGuests(rooms * 2));
+        } else {
+            dispatch(setGuests(value));
+        }
+    };
+    const handleVisibleChange = (visible) => {
+        setVisible(visible);
+    };
+    const content = (
+        <div>
+            <Row gutter={[16, 16]}>
+                <Col span={12}>
+                    <span>Guests</span>
+                </Col>
+                <Col span={12}>
+                    <InputNumber min={1} max={10} value={guests} onChange={handleGuestsChange} />
+                </Col>
+                <Col span={12}>
+                    <span>Rooms</span>
+                </Col>
+                <Col span={12}>
+                    <InputNumber min={1} max={10} value={rooms} onChange={handleRoomsChange} />
+                </Col>
+            </Row>
+
+        </div>
+    );
+    console.log("destination: " + destination);
+    console.log("guests: " + guests);
+    console.log("rooms: " + rooms);
+    console.log("date: " + date);
     const onChange = (e) => {
         console.log(e.target.value);
     };
@@ -24,7 +89,12 @@ const Filter = () => {
             console.log(err.message);
         }
     };
-
+    const handleDateChange = (dates) => {
+        dispatch(setDate(dates));
+    };
+    const handleDestinationChange = (value) => {
+        dispatch(setDestination(value));
+    };
     return (
         <Row justify="center" align="middle" className="Home-layout">
             <Col xs={24} md={12}>
@@ -47,18 +117,17 @@ const Filter = () => {
 
             <div className="search-layout-home">
                 <div className="search-content-container">
-                    <label className="search-content-container-label" style={{ fontFamily: 'DM Sans, sans-serif' }}>Key words</label>
-                    <Input
-                        placeholder="Type your keywords"
-                        value={keywords}
-                        onChange={(e) => setKeywords(e.target.value)}
-                        style={{ marginRight: '10px', border: 'none', color: '#8c8c8c', fontSize: '16px' }}
-                    />
+                    <label className="search-content-container-label" style={{ fontFamily: 'DM Sans, sans-serif' }}>Check In/Check Out</label>
+
+                    <Space direction="vertical" size={12}>
+
+                        <RangePicker disabledDate={disabledDate} format={dateFormat} onChange={handleDateChange} />
+                    </Space>
                 </div>
                 <div className="search-content-container">
-                    <label className="search-content-container-label">Destination</label>
+                    <label className="search-content-container-label">Location</label>
                     <Select
-                        onChange={(value) => setDestination(value)}
+                        onChange={handleDestinationChange}
                         showSearch
                         style={{
                             width: 200,
@@ -79,17 +148,19 @@ const Filter = () => {
                     </Select>
                 </div>
                 <div className="search-content-container">
-                    <label className="search-content-container-label">Duration</label>
-                    <Select
-                        placeholder="Duration"
-                        value={duration}
-                        onChange={(value) => setDuration(value)}
-                        style={{ width: '120px', marginRight: '10px' }}
+                    <label className="search-content-container-label">Guest and Room</label>
+
+                    <Popover
+                        content={content}
+                        title="Select Guests and Rooms"
+                        trigger="click"
+                        open={visible}
+                        onOpenChange={handleVisibleChange}
                     >
-                        {[1, 2, 3].map((day) => (
-                            <Option key={day}>{`${day} day${day > 1 ? 's' : ''}`}</Option>
-                        ))}
-                    </Select>
+                        <Button>
+                            {guests} Guests, {rooms} Room(s) <UserOutlined />
+                        </Button>
+                    </Popover>
                 </div>
                 <div className="search-layout-home-btn">
                     <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
